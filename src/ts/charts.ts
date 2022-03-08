458b6031d0d63503939bc3f25584f5390d51ece3
Georges.L <contact@geolim4.com>
2022-03-08 02:35:22 +0100

    Updated charts to better display peers as distinct datum

--- src/ts/charts.ts
+++ src/ts/charts.ts
@@ -20,15 +20,21 @@ export class Charts {
 
   protected static buildBarChartPerCriteria(markers: ExtendedGoogleMapsMarker[], filters: FormFilters, definitions: Definition[], criteria: string, year: string): void {
     const series = [];
+    const peersList = this.getPeersList(criteria, markers);
 
     for (const criteriaFilter of filters[criteria]) {
       const data = Array(12).fill(0);
       for (const marker of markers) {
         if (marker.death[criteria] === criteriaFilter.value) {
           data[parseInt(marker.death.month, 0) - 1] += marker.death.count;
-          for (const peer of marker.death.peers) {
-            data[parseInt(marker.death.month, 0) - 1] += peer.count;
-          }
+        }
+      }
+      /**
+       * Join the peers, if applicable
+       */
+      if (peersList[criteriaFilter.value]) {
+        for (const peerMonth in peersList[criteriaFilter.value]) {
+          data[parseInt(peerMonth, 0) - 1] += peersList[criteriaFilter.value][peerMonth];
         }
       }
       series.push({ data , name: criteriaFilter.label, color: this.getFilterCriteriaColor(criteria, criteriaFilter.value, filters) });
@@ -75,15 +81,21 @@ export class Charts {
 
   protected static buildPieChartPerCriteria(markers: ExtendedGoogleMapsMarker[], filters: FormFilters, definitions: Definition[], criteria: string, year: string): void {
     const seriesData = [];
+    const peersList = this.getPeersList(criteria, markers);
 
     for (const criteriaFilter of filters[criteria]) {
       let counter = 0;
       for (const marker of markers) {
         if (marker.death[criteria] === criteriaFilter.value) {
           counter += marker.death.count;
-          for (const peer of marker.death.peers) {
-            counter += peer.count;
-          }
+        }
+      }
+      /**
+       * Join the peers, if applicable
+       */
+      if (peersList[criteriaFilter.value]) {
+        for (const peerMonth in peersList[criteriaFilter.value]) {
+          counter += peersList[criteriaFilter.value][peerMonth];
         }
       }
       if (counter) {
@@ -117,6 +129,41 @@ export class Charts {
         categories: [],
       },
     });
+  }
+
+  protected static getPeersList(criteria: string, markers: ExtendedGoogleMapsMarker[]): { [name: string]: { [name: string]: number } } {
+    const peersList = <{ [name: string]: { [name: string]: number } }> {}; // @todo make Reusable interface for that object type
+
+    for (const marker of markers) {
+      if (marker.death.peers) {
+        for (const peer of marker.death.peers) {
+          if (typeof peer[criteria] !== 'undefined') {
+            if (typeof peersList[peer[criteria]] === 'undefined') {
+              peersList[peer[criteria]] = {};
+            }
+            if (typeof peersList[peer[criteria]][marker.death.month] === 'undefined') {
+              peersList[peer[criteria]][marker.death.month] = 0;
+            }
+            peersList[peer[criteria]][marker.death.month] += peer.count;
+
+            /**
+             * If the criteria does not exist in peer (e.g: "cause" criteria)
+             * the check in the parent object if it exists
+             */
+          } else if (typeof marker.death[criteria] !== 'undefined') {
+            if (typeof peersList[marker.death[criteria]] === 'undefined') {
+              peersList[marker.death[criteria]] = {};
+            }
+            if (typeof peersList[marker.death[criteria]][marker.death.month] === 'undefined') {
+              peersList[marker.death[criteria]][marker.death.month] = 0;
+            }
+            peersList[marker.death[criteria]][marker.death.month] += peer.count;
+          }
+        }
+      }
+    }
+
+    return peersList;
   }
 
   protected static getFilterCriteriaColor(criteria: string, value: string, filters: FormFilters): string {
