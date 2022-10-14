a748d1fa9e40999b95ba9b43659d3d0ba98f9326
Georges Limouzy <georges.limouzy@smile.fr>
2022-10-14 19:48:11 +0200

    Released new build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/** SW auto-generated on 2022-10-12T12:45:12.783Z **/
-const CACHE_KEY = 'im-3.3.0+20221012T124512';
+/** SW auto-generated on 2022-10-14T17:47:04.111Z **/
+const CACHE_KEY = 'im-3.3.1+20221014T174704';
 const assets = [
   'assets/css/fonts/glyphicons-halflings-regular.eot',
   'assets/css/fonts/glyphicons-halflings-regular.svg',
