0e90e90fcd57d6e6c8c0daddc961910064ec104a
Georges.L <contact@geolim4.com>
2022-10-24 02:01:20 +0200

    Released new build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/** SW auto-generated on 2022-10-23T14:45:44.509Z **/
-const CACHE_KEY = 'im-3.5.3+20221023T144544';
+/** SW auto-generated on 2022-10-24T00:00:46.436Z **/
+const CACHE_KEY = 'im-3.5.4+20221024T000046';
 const assets = [
   'assets/css/fonts/glyphicons-halflings-regular.eot',
   'assets/css/fonts/glyphicons-halflings-regular.svg',
