7babd3ee4a4f79bfbe4c3944380c63c9a9f052c7
Georges <contact@geolim4.com>
2023-03-06 02:38:35 +0100

    Released new build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/** SW auto-generated on 2023-02-27T19:36:47.513Z **/
-const CACHE_KEY = 'im-4.8.4+20230227T193647';
+/** SW auto-generated on 2023-03-06T01:38:17.656Z **/
+const CACHE_KEY = 'im-4.8.4+20230306T013817';
 const assets = [
   'assets/css/fonts/fa-solid-900.ttf',
   'assets/css/fonts/fa-solid-900.woff2',
