6ce4edb8d2489be56b415199502fe1ac836d26fe
Georges <contact@geolim4.com>
2023-09-11 05:19:23 +0200

    Added prev/next death link

--- src/ts/models/filteredResponse.model.ts
+++ src/ts/models/filteredResponse.model.ts
@@ -3,5 +3,6 @@ import { Bloodbath } from './bloodbath.model';
 export interface FilteredResponse {
     response: Bloodbath;
     errored: boolean;
+    error?: Error;
     original_response: Bloodbath;
 }
